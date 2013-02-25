17913f4deab56291d83628db573c82bdbd68612b
Will Sulzer <willsulzer@gmail.com>
2013-02-24 23:37:19 -0800

    hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NantucketTownRentals::Application.config.secret_token = '72dd80f3d09c28c302214743157229c47c278a5154f53ab0a07da82ab34cc8038a22010b18f82381ed58a70e090f58e7b6b4ddc5ea476bc9b04dc8534240faa2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NantucketTownRentals::Application.config.session_store :cookie_store, key: '_nantucket-town-rentals_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NantucketTownRentals::Application.config.session_store :active_record_store
